3e502fe013bb06110b9b4fb2ae41d5aef98482b2
nithinganji <nithinreddy.ganji@rsrit.com>
2018-09-05 16:32:19 -0400

    initial commit

---
+++ src/test/java/com/nithin/PcfEurekaDiscoveryClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nithin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PcfEurekaDiscoveryClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
